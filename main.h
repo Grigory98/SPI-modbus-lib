ba8330f8172aee764b6b79f92453b1a93d98d8ec
Grigory <73303452+Grigory98@users.noreply.github.com>
2020-10-22 20:59:18 +0400

    Delete main.h

--- main.h
+++
@@ -1,7 +0,0 @@
-extern uint16_t MB_holding_reg[16];
-extern uint16_t MB_input_reg[16];
-extern volatile unsigned char Massiv_input[20]; //ввод(запрос)
-extern volatile unsigned char Massiv_output[20]; //вывод(ответ)
-unsigned int RCR;
-void Timer_ini();
-unsigned char adress;
